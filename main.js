eacfa0a272489e60af4d3fb4bb1d9a8649b5376b
ebellotpu6 <ebellotpu6@gmail.com>
2022-03-11 11:14:48 +0100

    Developing the subCategory Menu: Managing when and how to display it. *Is still needed to enter the data of the subcategories correctly.*

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const navbar_button = document.getElementById("navbar_button");
 const sideBarMenu = document.getElementById("navbar_sidePanel_container");
 const close_navbar_button = document.getElementById("close_navbar_button");
+const navbar_category_container = document.getElementById("navbar_category_container");
+const navbar_subMenu = document.querySelectorAll('.navbar-subMenu');
+const navbar_categoryOverlayConatainer = document.getElementById("navbar_categoryOverlayConatainer");
+const navbar_subMenuImage = document.getElementById("navbar_subMenuImage");
 
 navbar_button.addEventListener("click", function(){
     console.log("show sidebar menu");
@@ -12,4 +16,19 @@ close_navbar_button.addEventListener("click", function(){
     sideBarMenu.style = "transform: translate3d(-100%, 0, 0);";
 });
 
+navbar_category_container.addEventListener("mouseleave", function(){
+    console.log("hide submenu");
+    navbar_categoryOverlayConatainer.classList.remove("navbar_categoryOverlayConatainer-open");
+});
 
+
+function showSubmenu(category) {
+    navbar_categoryOverlayConatainer.classList.add("navbar_categoryOverlayConatainer-open");
+    for(const element of navbar_subMenu){
+        if (element.attributes.reference.value == category){
+            element.style = "display: grid;"
+            navbar_subMenuImage.style=`background-image: url(./src/images/categories/${category}.jpeg);`
+        }
+        else element.style = "display: none";
+    }
+}
